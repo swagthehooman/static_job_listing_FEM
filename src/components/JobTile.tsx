f716eb398acaa7548c4f12e411e202b95f7e5291
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2024-06-23 19:18:03 +0530

    desktop design complete

--- src/components/JobTile.tsx
+++ src/components/JobTile.tsx
@@ -2,9 +2,9 @@ import LanguagesButton from "./LanguagesButton"
 
 const JobTile = ({ jobtileProp }: { jobtileProp: jobtile }) => {
     return (
-        <div className="w-[90%] ml-4 relative mt-16 border-l-darkCyan border-l-8 p-4 pt-8 rounded-md shadow-md bg-white" key={jobtileProp.id}>
-            <img src={jobtileProp.logo} alt="companyLogo" className="w-12 h-12 absolute -top-[10%]" />
-            <div className="ml-auto flex flex-col gap-2">
+        <div className="w-[90%] ml-4 relative mt-16 border-l-darkCyan border-l-8 p-4 pt-8 rounded-md shadow-md bg-white min-[600px]:flex min-[600px]:w-auto min-[600px]:item-center min-[600px]:pt-4 min-[600px]:justify-between" key={jobtileProp.id}>
+            <img src={jobtileProp.logo} alt="companyLogo" className="w-12 h-12 absolute -top-[10%] min-[600px]:top-1/2 min-[600px]:-translate-y-1/2 min-[600px]:w-20 min-[600px]:h-20" />
+            <div className="flex flex-col gap-2 min-[600px]:justify-between min-[600px]:ml-24">
                 <div className="flex gap-4 items-center">
                     <p className="text-darkCyan font-semibold">{jobtileProp.company}</p>
                     {jobtileProp.new && <p className="pt-1 pr-2 pl-2 rounded-full bg-darkCyan text-lightGrayCyanFilter text-sm font-semibold">NEW!</p>}
@@ -20,7 +20,7 @@ const JobTile = ({ jobtileProp }: { jobtileProp: jobtile }) => {
                 </div>
                 <hr className="hidden h-2 max-[600px]:block"></hr>
             </div>
-            <div className="flex gap-2 flex-wrap">
+            <div className="flex gap-2 flex-wrap min-[600px]:flex-nowrap min-[600px]:items-center">
                 <LanguagesButton prop={jobtileProp.role} />
                 <LanguagesButton prop={jobtileProp.level} />
                 {jobtileProp.languages.map(i => <LanguagesButton prop={i} />)}
